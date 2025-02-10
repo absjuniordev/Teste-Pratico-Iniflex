21ae9d00292be9811a4fd7e33a7e7e958e53a677
Arnaldo Junior <abs.junnior@hotmail.com>
2025-02-10 11:45:40 -0300

    Criação da entidade Pessoa

---
+++ src/main/java/com/absjunniordev/model/Pessoa.java
@@ -0,0 +1,35 @@
+package com.absjunniordev.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Pessoa {
+
+    private String name;
+    private LocalDate dataNascimento;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String name, LocalDate dataNascimento) {
+        this.name = name;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDataNascimento() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return dataNascimento.format(formatter);
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
