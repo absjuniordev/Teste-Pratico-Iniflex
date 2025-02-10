83c5a03d9fe61db5c3c7e78a8e504b9659812521
Arnaldo Junior <abs.junnior@hotmail.com>
2025-02-10 17:41:24 -0300

    Criação da entidade Funcionario

---
+++ src/main/java/com/absjunniordev/model/Funcionario.java
@@ -0,0 +1,38 @@
+package com.absjunniordev.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa{
+
+    private BigDecimal salario;
+    private String funcao;
+
+    Funcionario(){}
+
+    public Funcionario(String name, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(name, dataNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public void aumentarSalario(BigDecimal percentual) {
+        this.salario = salario.add(salario.multiply(percentual));
+    }
+}
